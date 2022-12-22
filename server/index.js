a64a0edf554464341316154e3c00ed439167d75a
mohammedabdulhady <m.abdulhady@i-esnaad.com>
2022-12-22 16:03:31 +0200

    Setting up mongodb connection

--- server/index.js
+++ server/index.js
@@ -3,8 +3,16 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT | 3001;
 
-const app = express();
+const DB = "mongodb://mo15hammed:test1234@ac-o3brnfv-shard-00-00.mfpttzg.mongodb.net:27017,ac-o3brnfv-shard-00-01.mfpttzg.mongodb.net:27017,ac-o3brnfv-shard-00-02.mfpttzg.mongodb.net:27017/?ssl=true&replicaSet=atlas-pjb91r-shard-0&authSource=admin&retryWrites=true&w=majority";
+mongoose.set('strictQuery', false);
+mongoose.connect(DB).then(() => {
+    console.log('DB Connected successfully!');
+}).catch((e) => {
+    console.log(e);
+});
 
+const app = express();
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`CONNECTED at port ${PORT}`);
+
 });
